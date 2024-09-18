05af09f45f0c37a0441c833fd5f654eae525f5ed
John Brittain <jsb1982@gmail.com>
2024-09-18 16:08:35 +0100

    Annotate Hello module example

--- workflows/Hello/sources/Hello/.test.sh
+++ workflows/Hello/sources/Hello/.test.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+#
+# This script is used to test the pipeline
+#
+# We use a script to provide maximum flexibility in testing (remove old results, etc.)
+# though they will generally call snakemake to execute the _test rule with a
+# test configuration file.
 
+# Remove any old results
 rm -rf results/out
+
+# Run the pipeline, specifying a test configuration file and target rule `_test`
 snakemake --cores 1 --configfile=config/.test.yaml _test
